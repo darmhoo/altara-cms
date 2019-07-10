3755355df667343771eb507232e0581c0ab6b333
darmhoo <darmhoo@hotmail.com>
2019-07-10 12:12:58 +0100

    User can login test is producing right output

--- tests/Feature/ApiLoginTest.php
+++ tests/Feature/ApiLoginTest.php
@@ -24,14 +24,35 @@ class ApiLoginTest extends TestCase
     public function testUserCanLoginWithCorrectInfo(){
         $user = factory(User::class)->create([
             'password' => bcrypt($password = 'larami'),
+
         ]);
 
-        $response = $this->post('/login', [
+        $response = $this->post('http://localhost:8000/api/login', [
             'email' => $user->email,
             'password' => $password,
         ]);
 
-        $response->assertRedirect('/home');
-        $this->assertAuthenticatedAs($user);
+
+        $response->assertStatus(200);
+        $response->assertJson([
+            'authenticated' => true,
+            'username' => $user->first_name.' '.$user->last_name,
+            'user_id' => $user->id,
+
+            ]);
     }
+    public function testUserCannotLoginWithWrongInfo(){
+        $user = factory(User::class)->create([
+            'password' => bcrypt($password = 'larami'),
+
+        ]);
+        $response = $this->post('http://localhost:8000/api/login', [
+            'email' => $user->email,
+            'password' => $user->password,
+        ]);
+        $response->assertJson([
+            'email' => ['Provided email and password does not match!']
+        ]);
+    }
+
 }
